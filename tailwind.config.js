8daf28a4766f5f8d7475f5142d5be74be4deb470
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-12-02 22:40:34 +0530

    kando: Extend the tailwind color palette instead of overwriting it

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,12 @@ module.exports = {
             },
             backgroundImage: {
                 'main-img': "var(--main-img)",
-            }
-        },
-        colors: {
-            'main': 'var(--bg-clr)',
-            'accent': 'var(--acc-clr)',
-            'accent-alt': 'var(--acc-clr-alt)',
-            'white': '#fff'
+            },
+            colors: {
+                'main': 'var(--bg-clr)',
+                'accent': 'var(--acc-clr)',
+                'accent-alt': 'var(--acc-clr-alt)',
+            },
         },
     },
     plugins: [],
