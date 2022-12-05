fb85ff66c724344299b590c768a96030692ff57a
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-12-05 15:43:32 +0530

    kando: Add functionality to toggle sidebar

--- src/app.js
+++ src/app.js
@@ -3,3 +3,28 @@ import 'iconify-icon';
 import dataController from "./js/lib/DataController";
 
 dataController.init();
+
+const contentWrapper = document.getElementById('content-wrapper');
+const sidebar = document.getElementById('sidebar');
+const sidebarBtn = document.getElementById('sidebar-btn');
+
+sidebarBtn.addEventListener('click', toggleSidebar);
+
+function toggleSidebar() {
+    const state = () => sidebar.getAttribute('aria-expanded') === 'true';
+    const setState = (newState) => sidebar.setAttribute('aria-expanded', newState);
+
+    // Toggle sidebar state
+    (state()) ? setState(false) : setState(true);
+
+    // When navbar is closing hide the overflow on the body
+    if (!state()) {
+        document.body.style.overflow = 'hidden';
+        sidebar.addEventListener('transitionend', () => {
+            document.body.style.overflow = 'visible';
+        }, { once: true });
+    }
+
+    // Shift the content when sidebar is open
+    contentWrapper.classList.toggle('sidebar-is-open');
+}
