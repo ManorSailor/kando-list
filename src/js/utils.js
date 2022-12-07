295777a045e907a04d966b3a1957e2bdd3e85b68
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-12-07 19:19:43 +0530

    kando: Add support to return firstElementChild in createElement

--- src/js/utils.js
+++ src/js/utils.js
@@ -7,6 +7,19 @@ function makeIdGen(initialVal = 0) {
     return () => initialVal++;
 }
 
+/**
+ * Convert HTML string literal into DOM Node(s), returns a DocumentFragment
+ * @param {String} htmlStr
+ * @param {Boolean} firstElementChild
+ * @returns {DocumentFragment}
+ */
+function createElement(htmlStr, firstElementChild = true) {
+    const range = document.createRange();
+    const docFrag = range.createContextualFragment(htmlStr);
+    return (firstElementChild) ? docFrag.firstElementChild : docFrag;
+}
+
 export {
     makeIdGen,
+    createElement,
 }
