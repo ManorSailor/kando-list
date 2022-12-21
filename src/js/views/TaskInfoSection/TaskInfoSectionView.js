7c8fa6d84a61664f3df313115665a5b1b78d5434
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-12-21 19:23:38 +0530

    kando: Refactor TaskInfoSectionView to use 'ACTIVE_TASK_CHANGED' event

--- src/js/views/TaskInfoSection/TaskInfoSectionView.js
+++ src/js/views/TaskInfoSection/TaskInfoSectionView.js
@@ -4,6 +4,22 @@ import TaskInfoViews from "./TaskInfoViews";
 const taskInfoBody = createElement(`<!-- Task Info --><section class="bg-main w-full min-w-[300px] max-w-xl flex-grow flex p-4 rounded-2xl shadow-md overflow-hidden"></section>`);
 
 /**
+ * Generate TaskInfoViews & render the task info in the passed Node
+ * @param {Task} task 
+ * @param {List} list 
+ * @param {Node} TaskInfoSection 
+ * @param {Function} removeTaskHandler 
+ */
+function renderInfo(task, list, TaskInfoSection, removeTaskHandler) {
+    clearNode(TaskInfoSection);
+
+    if (task) {
+        const taskInfoViews = TaskInfoViews(task, list, removeTaskHandler);
+        TaskInfoSection.appendChild(taskInfoViews);
+    }
+}
+
+/**
  * Generates a TaskInfoSection on a per list basis
  * @param {List} list
  * @param {Function} removeTaskHandler 
@@ -13,16 +29,13 @@ function TaskInfoSection(list, removeTaskHandler) {
     const TaskInfoSection = taskInfoBody.cloneNode(true);
 
     const taskObserver = {
-        update: (data) => {
-            clearNode(TaskInfoSection);
-            const taskInfoViews = TaskInfoViews(data.task, list, removeTaskHandler);
-            TaskInfoSection.appendChild(taskInfoViews);
-        }
+        update: (data) => renderInfo(data.task, list, TaskInfoSection, removeTaskHandler),
     };
 
-    list.addObserver('TASK_REMOVED', taskObserver);
     list.addObserver('ACTIVE_TASK_CHANGED', taskObserver);
 
+    renderInfo(list.activeTask, list, TaskInfoSection, removeTaskHandler);
+    
     return TaskInfoSection;
 }
 
