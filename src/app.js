9e3b97a439a299855105ebe0923a210d5440ffee
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-12-28 15:46:32 +0530

    kando: Refactor DataController to reduce useless imports

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ import Kando from "./js/models/Kando/Kando";
 import List from "./js/models/List/List";
 import Task from "./js/models/Task/Task";
 
+// Initialize a new Kando Instance
+const kando = new Kando();
+
 // Assigning modules to window object, can be accessed through browser console
 // webpack encapsulates everything into a module to stop polluting global scope
 window.Task = Task;
 window.List = List;
-window.Kando = Kando;
+window.Kando = kando;
 
-dataController.init();
+dataController.init(kando, List, Task);
--- src/js/lib/DataController.js
+++ src/js/lib/DataController.js
@@ -1,31 +1,46 @@
 import LocalDBManager from "../db/LocalDB";
 import dataObserver from "./DataObserver";
-import Kando from "../models/Kando/Kando";
-import List from "../models/List/List";
-import Task from "../models/Task/Task";
 
-function init() {
+/**
+ * DataController initializer
+ * @param {Kando} kando 
+ * @param {Constructor} List 
+ * @param {Constructor} Task 
+ */
+function init(kando, List, Task) {
     const localdb = new LocalDBManager('kando');
     
     const appData = localdb.fetch();
 
     if (appData.lists?.length) {
-        reloadAppState(appData);
+        reloadAppState(kando, List, Task, appData);
     } else {
-        const defaultList = new List({ name: 'Personal' });
-        Kando.addList(defaultList);
-        localdb.save(Kando);
+        initDefaultState(kando, localdb);
     }
 
     // Initialize data observer. Bind context of save method to localdb
-    dataObserver.init(localdb.save.bind(localdb), Kando);
+    dataObserver.init(localdb.save.bind(localdb), kando);
+}
+
+/**
+ * Initialize default app state through a pre-defined list
+ * @param {Kando} kando 
+ * @param {LocalDBManager} localdb 
+ */
+function initDefaultState(kando, localdb) {
+    const defaultList = new List({ name: 'Personal' });
+    kando.addList(defaultList);
+    localdb.save(kando);
 }
 
 /**
  * Reload app state in memory by converting objects into proper types
+ * @param {Kando} kando 
+ * @param {Constructor} List 
+ * @param {Constructor} Task 
  * @param {Object} appData 
  */
-function reloadAppState(appData) {
+function reloadAppState(kando, List, Task, appData) {
     const lists = appData.lists;
 
     lists.forEach(listObj => {
@@ -36,11 +51,11 @@ function reloadAppState(appData) {
             list.addTask(task);
         });
 
-        Kando.addList(list);
+        kando.addList(list);
     });
 
     // Set the activeList to firstList on page Load
-    Kando.activeList = Kando.firstList;
+    kando.activeList = kando.firstList;
 }
 
 const dataController = { init };
