cf815fbe2949384c61b12f490c2d43eefa40a9ac
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-12-31 22:43:21 +0530

    kando: Implement notes functionality

--- src/js/views/TaskInfoSection/TaskNotes/TaskNotesView.js
+++ src/js/views/TaskInfoSection/TaskNotes/TaskNotesView.js
@@ -1,10 +1,10 @@
 import { createElement } from "../../../utils";
 
-const taskNotesBody = createElement(`
+const taskNotesBody = ({ notes }) => createElement(`
 <!-- Task Notes -->
 <div class="flex flex-col gap-2 justify-start">
     <p class="text-gray-400 text-xs font-semibold tracking-widest ml-2 select-none">NOTES</p>
-    <input id="task-notes" class="bg-transparent px-2 py-1 hover:bg-black focus-within:bg-black transition-colors duration-200 outline-none rounded-lg caret-accent" type="text" placeholder="Insert your notes here">
+    <input id="task-notes" class="bg-transparent px-2 py-1 hover:bg-black focus-within:bg-black transition-colors duration-200 outline-none rounded-lg caret-accent" type="text" placeholder="Insert your notes here" value="${(notes) ? notes : ''}">
 </div>
 `);
 
@@ -14,7 +14,21 @@ const taskNotesBody = createElement(`
  * @returns {Node}
  */
 function TaskNotesView(task) {
-    const taskInfoTitle = taskNotesBody.cloneNode(true);
+    const taskInfoTitle = taskNotesBody(task);
+
+    const notesInput = taskInfoTitle.querySelector('input');
+
+    notesInput.addEventListener('change', (e) => {
+        const notes = e.target.value;
+        task.notes = notes;
+    });
+
+    const notesObserver = {
+        update: (data) => notesInput.textContent = data.notes,
+    }
+
+    task.addObserver('TASK_NOTES_CHANGED', notesObserver);
+
     return taskInfoTitle;
 }
 
