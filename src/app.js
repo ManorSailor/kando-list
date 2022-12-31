7697147b37854695ea929d3ba4bf8e4f34294a69
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-12-31 17:20:56 +0530

    kando: Implement DetailedTask model

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import dataController from "./js/lib/DataController";
 import Kando from "./js/models/Kando/Kando";
 import List from "./js/models/List/List";
-import Task from "./js/models/Task/Task";
+import DetailedTask from "./js/models/Task/DetailedTask";
 
 // Initialize a new Kando Instance
 const kando = new Kando();
 
 // Assigning modules to window object, can be accessed through browser console
 // webpack encapsulates everything into a module to stop polluting global scope
-window.Task = Task;
+window.Task = DetailedTask;
 window.List = List;
 window.Kando = kando;
 
-dataController.init(kando, List, Task);
+dataController.init(kando, List, DetailedTask);
